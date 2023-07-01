82f55705b4868f9b5c84bc36efda5d5bebf8f523
Nicole Tan <geniusnic009@gmail.com>
2023-07-01 09:02:33 +0800

    Initial Commit

---
+++ grp proj/grp_projApp.swift
@@ -0,0 +1,17 @@
+//
+//  grp_projApp.swift
+//  grp proj
+//
+//  Created by T Krobot on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct grp_projApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
