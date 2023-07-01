00d800adc3f8de5862da221dec8fa19bc537356f
Nicole Tan <geniusnic009@gmail.com>
2023-07-01 10:14:56 +0800

    added first button

--- grp proj/ContentView.swift
+++ grp proj/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showSpeach = false
+    @State private var limit = 0
     var body: some View {
         ZStack {
             Color(red: 188/255, green: 227/255, blue: 255/255)
@@ -16,7 +18,17 @@ struct ContentView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200)
-                
+                Button {
+                    showSpeach = true
+                    limit += 1
+                } label: {
+                    Text("How tall are penguins")
+                        .padding(20)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                        .background(Color(red: 114/255, green: 178/255, blue: 224/255))
+                        .cornerRadius(10)
+                }
             }
             .padding()
         }
