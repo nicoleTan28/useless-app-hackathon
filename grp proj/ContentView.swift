3adc1031a958219ec6d2088e5765efb3d1cdbdc4
Nicole Tan <geniusnic009@gmail.com>
2023-07-01 10:26:18 +0800

    part 1/3 of penguin's response to question (used variables showSpeach and limit)

--- grp proj/ContentView.swift
+++ grp proj/ContentView.swift
@@ -8,23 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showSpeach = false
+    @State private var showSpeach = 0
     @State private var limit = 0
     var body: some View {
         ZStack {
             Color(red: 188/255, green: 227/255, blue: 255/255)
             VStack {
+                if showSpeach == 1 {
+                    Text("You're asking *me*, (penguin name), about penguin facts? Is that all I am to you? Do you think I was created to answer your trivial trivia questions?")
+                        .padding()
+                        .font(.title)
+                        .background()
+                        .cornerRadius(15)
+                }
+
                 Image("penguinPng")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200)
                 Button {
-                    showSpeach = true
+                    showSpeach += 1
                     limit += 1
                 } label: {
                     Text("How tall are penguins")
                         .padding(20)
-                        .font(.largeTitle)
+                        .font(.title)
                         .foregroundColor(.black)
                         .background(Color(red: 114/255, green: 178/255, blue: 224/255))
                         .cornerRadius(10)
